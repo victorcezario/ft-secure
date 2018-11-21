72b24cff1a58d68cc81859c9478c94f82cb97701
unknown <victorhcezario@gmail.com>
2018-11-21 21:04:59 -0200

    Funções files

--- faceTraining.py
+++ faceTraining.py
@@ -3,35 +3,36 @@ import numpy as np
 from PIL import Image
 import os
 
-path = 'dataset'
+def training():
+    path = 'dataset'
 
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-detector = cv2.CascadeClassifier("train/haarcascade_frontalface_default.xml");
+    recognizer = cv2.face.LBPHFaceRecognizer_create()
+    detector = cv2.CascadeClassifier("train/haarcascade_frontalface_default.xml");
 
-def getImagesAndLabels(path):
+    def getImagesAndLabels(path):
 
-    imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
-    faceSamples=[]
-    ids = []
+        imagePaths = [os.path.join(path,f) for f in os.listdir(path)]     
+        faceSamples=[]
+        ids = []
 
-    for imagePath in imagePaths:
+        for imagePath in imagePaths:
 
-        PIL_img = Image.open(imagePath).convert('L')
-        img_numpy = np.array(PIL_img,'uint8')
+            PIL_img = Image.open(imagePath).convert('L')
+            img_numpy = np.array(PIL_img,'uint8')
 
-        id = int(os.path.split(imagePath)[-1].split(".")[1])
-        faces = detector.detectMultiScale(img_numpy)
+            id = int(os.path.split(imagePath)[-1].split(".")[1])
+            faces = detector.detectMultiScale(img_numpy)
 
-        for (x,y,w,h) in faces:
-            faceSamples.append(img_numpy[y:y+h,x:x+w])
-            ids.append(id)
+            for (x,y,w,h) in faces:
+                faceSamples.append(img_numpy[y:y+h,x:x+w])
+                ids.append(id)
 
-    return faceSamples,ids
+        return faceSamples,ids
 
-print ("\n [INFO] Treinando faces. Aguarde ...")
-faces,ids = getImagesAndLabels(path)
-recognizer.train(faces, np.array(ids))
+    print ("\n [INFO] Treinando faces. Aguarde ...")
+    faces,ids = getImagesAndLabels(path)
+    recognizer.train(faces, np.array(ids))
 
-recognizer.write('train/trainer.yml')
+    recognizer.write('train/trainer.yml')
 
-print("\n [INFO] {0} faces treinadas. Finalizando o Programa".format(len(np.unique(ids))))
+    print("\n [INFO] {0} faces treinadas. Finalizando o Programa".format(len(np.unique(ids))))
