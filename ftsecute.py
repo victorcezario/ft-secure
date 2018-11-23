432a93d8ebf5f7234806e6ea2f7b2938461c4daa
unknown <victorhcezario@gmail.com>
2018-11-23 20:20:18 -0200

    teste

--- ftsecute.py
+++ ftsecute.py
@@ -37,23 +37,68 @@ def writeDisplay(texto):
     RST = 24
     SPI_PORT = 0
     SPI_DEVICE = 0
+
+    # Raspberry Pi software SPI config:
+    # SCLK = 4
+    # DIN = 17
+    # DC = 23
+    # RST = 24
+    # CS = 8
+
+    # Beaglebone Black hardware SPI config:
+    # DC = 'P9_15'
+    # RST = 'P9_12'
+    # SPI_PORT = 1
+    # SPI_DEVICE = 0
+
+    # Beaglebone Black software SPI config:
+    # DC = 'P9_15'
+    # RST = 'P9_12'
+    # SCLK = 'P8_7'
+    # DIN = 'P8_9'
+    # CS = 'P8_11'
+
+
     # Hardware SPI usage:
     disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
 
+    # Software SPI usage (defaults to bit-bang SPI interface):
+    #disp = LCD.PCD8544(DC, RST, SCLK, DIN, CS)
+
     # Initialize library.
     disp.begin(contrast=60)
+
     # Clear display.
     disp.clear()
     disp.display()
+
+    # Create blank image for drawing.
+    # Make sure to create image with mode '1' for 1-bit color.
     image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+
     # Get drawing object to draw on image.
     draw = ImageDraw.Draw(image)
+
     # Draw a white filled box to clear the image.
     draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
+
+    # Draw some shapes.
+    draw.ellipse((2,2,22,22), outline=0, fill=255)
+    draw.rectangle((24,2,44,22), outline=0, fill=255)
+    draw.polygon([(46,22), (56,2), (66,22)], outline=0, fill=255)
+    draw.line((68,22,81,2), fill=0)
+    draw.line((68,2,81,22), fill=0)
+
     # Load default font.
     font = ImageFont.load_default()
+
+    # Alternatively load a TTF font.
+    # Some nice fonts to try: http://www.dafont.com/bitmap.php
+    # font = ImageFont.truetype('Minecraftia.ttf', 8)
+
     # Write some text.
-    draw.text((2,10), texto, font=font)
+    draw.text((8,30), 'Hello World!', font=font)
+
     # Display image.
     disp.image(image)
     disp.display()
