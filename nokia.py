7abeb09f7008ab151f6fec81483e2998b1d6d7ca
unknown <victorhcezario@gmail.com>
2018-11-23 20:48:00 -0200

    okok

--- nokia.py
+++ nokia.py
@@ -1,4 +1,5 @@
 #Bibliotecas Nokia 5110
+import sys
 import time
 import Adafruit_Nokia_LCD as LCD
 import Adafruit_GPIO.SPI as SPI
@@ -6,30 +7,27 @@ from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
-#Function escreve display
-def writeDisplay(texto):
-    # Raspberry Pi hardware SPI config:
-    DC = 23
-    RST = 24
-    SPI_PORT = 0
-    SPI_DEVICE = 0
-    # Hardware SPI usage:
-    disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
-
-    # Initialize library.
-    disp.begin(contrast=60)
-    # Clear display.
-    disp.clear()
-    disp.display()
-    image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
-    # Draw a white filled box to clear the image.
-    draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
-    # Load default font.
-    font = ImageFont.load_default()
-    # Write some text.
-    draw.text((2,10), texto, font=font)
-    # Display image.
-    disp.image(image)
-    disp.display()
+# Raspberry Pi hardware SPI config:
+DC = 23
+RST = 24
+SPI_PORT = 0
+SPI_DEVICE = 0
+# Hardware SPI usage:
+disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
+# Initialize library.
+disp.begin(contrast=60)
+# Clear display.
+disp.clear()
+disp.display()
+image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+# Draw a white filled box to clear the image.
+draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
+# Load default font.
+font = ImageFont.load_default()
+# Write some text.
+draw.text((2,10), texto, font=font)
+# Display image.
+disp.image(image)
+disp.display()
