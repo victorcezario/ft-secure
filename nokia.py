7dc85a61821712cee0ed7a113f75792b904aa2c2
unknown <victorhcezario@gmail.com>
2018-11-23 20:48:24 -0200

    xxx

--- nokia.py
+++ nokia.py
@@ -27,7 +27,7 @@ draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
 # Load default font.
 font = ImageFont.load_default()
 # Write some text.
-draw.text((2,10), texto, font=font)
+draw.text((2,10), "xxxxxxx", font=font)
 # Display image.
 disp.image(image)
 disp.display()
