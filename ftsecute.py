817d5594e3196a2e4a720b547f444a3c2dec7de1
unknown <victorhcezario@gmail.com>
2018-11-23 20:31:30 -0200

    teste

--- ftsecute.py
+++ ftsecute.py
@@ -1,11 +1,4 @@
 # coding=utf-8
-#Bibliotecas Nokia 5110
-import time
-import Adafruit_Nokia_LCD as LCD
-import Adafruit_GPIO.SPI as SPI
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
 
 #Bibliotecas OpenCV
 import cv2
@@ -15,7 +8,7 @@ import glob
 import faceDataset
 import faceTraining
 import faceRecognition
-
+import nokia
 #LEDS
 import RPi.GPIO as GPIO
 
@@ -23,48 +16,13 @@ import RPi.GPIO as GPIO
 from num2words import num2words
 from subprocess import call
 
-# Define Status dos botões
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Botão GPIO-25
-GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Botão GPIO-8
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Botão GPIO-7
-
-#Function escreve display
-def writeDisplay(texto):
-    # Raspberry Pi hardware SPI config:
-    DC = 23
-    RST = 24
-    SPI_PORT = 0
-    SPI_DEVICE = 0
-    # Hardware SPI usage:
-    disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
-
-    # Initialize library.
-    disp.begin(contrast=60)
-    # Clear display.
-    disp.clear()
-    disp.display()
-    image = Image.new('1', (LCD.LCDWIDTH, LCD.LCDHEIGHT))
-    # Get drawing object to draw on image.
-    draw = ImageDraw.Draw(image)
-    # Draw a white filled box to clear the image.
-    draw.rectangle((0,0,LCD.LCDWIDTH,LCD.LCDHEIGHT), outline=255, fill=255)
-    # Load default font.
-    font = ImageFont.load_default()
-    # Write some text.
-    draw.text((2,10), texto, font=font)
-    # Display image.
-    disp.image(image)
-    disp.display()
-
 #Function falar palavras
 def speakText(texto):
     cmd_beg= 'espeak -vpt-br "'
     cmd_end= '" 2>/dev/null'
     call([cmd_beg+texto+cmd_end], shell=True)
 
-writeDisplay("FT SECURE")
+nokia.writeDisplay("FT SECURE")
 
 try:
     while True:
@@ -73,17 +31,17 @@ try:
          button_tree_state = GPIO.input(7)
          #Ação do Botão 1
          if button_one_state == False:
-            writeDisplay("FT SECURE")
+            nokia.writeDisplay("FT SECURE")
             speakText("Ola Seja bem vindo ao FT Secure")
             #faceRecognition.main()
             print('Pressione Ctrl-C para sair.')
          elif button_two_state == False:
-            writeDisplay("FT SECURE")
+            nokia.writeDisplay("FT SECURE")
             speakText("Ola Seja bem vindo ao FT Secure")
             #faceRecognition.main()
             print('Pressione Ctrl-C para sair.')
          elif button_tree_state == False:
-            writeDisplay("FT SECURE")
+            nokia.writeDisplay("FT SECURE")
             speakText("Ola Seja bem vindo ao FT Secure")
             #faceRecognition.main()
             print('Pressione Ctrl-C para sair.')
