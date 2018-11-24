a50346137f0a4b4df9d3dc45c2bc3fb2c333f21d
unknown <victorhcezario@gmail.com>
2018-11-23 22:13:28 -0200

    okok

--- ftsecute.py
+++ ftsecute.py
@@ -9,20 +9,12 @@ import faceDataset
 import faceTraining
 import faceRecognition
 #import nokia
-#LEDS
-import RPi.GPIO as GPIO
 
 #ESPEAK
 from num2words import num2words
 from subprocess import call
 
-GPIO.setmode(GPIO.BCM)
-# Define Status dos botões
-GPIO.setup(2, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Botão GPIO-25
-GPIO.setup(8, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Botão GPIO-8
-GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_UP) # Botão GPIO-7
-
-
+from gpiozero import Button
 #Function falar palavras
 def speakText(texto):
     cmd_beg= 'espeak -vpt-br "'
@@ -35,32 +27,31 @@ def writeDisplay(texto):
     cmd_beg= 'python /home/pi/positivo-ft-secure/nokia.py "'
     cmd_end= '" 2>/dev/null'
     call([cmd_beg+texto+cmd_end], shell=True)
-GPIO.cleanup()
+
 writeDisplay("Bem Vindo")
 speakText("Ola Seja bem vindo ao FT Secure")
 try:
     while True:
-         button_one_state = GPIO.input(2)
-         button_two_state = GPIO.input(8)
-         button_tree_state = GPIO.input(7)
-         GPIO.cleanup()
+         button_one_state = Button(2)
+         button_two_state = Button(8)
+         button_tree_state = Button(7)
          #Ação do Botão 1
-         if button_one_state == False:
+         if button_one_state.is_pressed:
             #nokia.writeDisplay("FT SECURE")
             speakText("Ola Seja bem vindo ao FT Secure")
             writeDisplay("FT Secure")
             #faceRecognition.main()
             print('Pressione Ctrl-C para sair.')
-         elif button_two_state == False:
+         elif button_two_state.is_pressed:
             #nokia.writeDisplay("FT SECURE")
             speakText("Ola Seja bem vindo ao FT Secure")
             #faceRecognition.main()
             print('Pressione Ctrl-C para sair.')
-         elif button_tree_state == False:
+         elif button_tree_state.is_pressed:
             #nokia.writeDisplay("FT SECURE")
             speakText("Ola Seja bem vindo ao FT Secure")
             #faceRecognition.main()
             print('Pressione Ctrl-C para sair.')
             
 except:
-    GPIO.cleanup()
+    print("Final")
