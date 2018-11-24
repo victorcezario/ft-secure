1fbd76f0e40de1122a5ce6de6367cc09623b5f30
unknown <victorhcezario@gmail.com>
2018-11-23 22:25:35 -0200

    Teste

--- ftsecute.py
+++ ftsecute.py
@@ -15,6 +15,7 @@ from num2words import num2words
 from subprocess import call
 
 from gpiozero import Button
+from signal import pause
 #Function falar palavras
 def speakText(texto):
     cmd_beg= 'espeak -vpt-br "'
@@ -28,28 +29,17 @@ def writeDisplay(texto):
     cmd_end= '" 2>/dev/null'
     call([cmd_beg+texto+cmd_end], shell=True)
 
+def reconhecimento():
+      speakText("Reconhecimento Iniciado")
+      writeDisplay("Reconhecimento")
+
 writeDisplay("Bem Vindo")
 speakText("Ola Seja bem vindo ao FT Secure")
 
-button_one_state = Button(2)
-button_two_state = Button(8)
-button_tree_state = Button(7)
-while True:
+button_one = Button(2)
+button_two = Button(8)
+button_tree = Button(7)
 
-   #Ação do Botão 1
-   if button_one_state.is_pressed:
-      #nokia.writeDisplay("FT SECURE")
-      speakText("Ola Seja bem vindo ao FT Secure")
-      writeDisplay("FT Secure")
-      #faceRecognition.main()
-      print('Pressione Ctrl-C para sair.')
-   elif button_two_state.is_pressed:
-      #nokia.writeDisplay("FT SECURE")
-      speakText("Ola Seja bem vindo ao FT Secure")
-      #faceRecognition.main()
-      print('Pressione Ctrl-C para sair.')
-   elif button_tree_state.is_pressed:
-      #nokia.writeDisplay("FT SECURE")
-      speakText("Ola Seja bem vindo ao FT Secure")
-      #faceRecognition.main()
-      print('Pressione Ctrl-C para sair.')
+button_one.when_pressed = reconhecimento
+
+pause()
