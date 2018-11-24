056dd1f590bbc14e75aff82e721416365ff2ede1
unknown <victorhcezario@gmail.com>
2018-11-24 14:46:43 -0200

    dataset

--- faceDataset.py
+++ faceDataset.py
@@ -11,46 +11,47 @@ Developed by Marcelo Rovai - MJRoBot.org @ 21Feb18
 
 import cv2
 import os
+def main():
+    cam = cv2.VideoCapture(0)
+    cam.set(3, 640) # set video width
+    cam.set(4, 480) # set video height
 
-cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video width
-cam.set(4, 480) # set video height
+    face_detector = cv2.CascadeClassifier('train/haarcascade_frontalface_default.xml')
 
-face_detector = cv2.CascadeClassifier('train/haarcascade_frontalface_default.xml')
+    # For each person, enter one numeric face id
+    face_id = 1
 
-# For each person, enter one numeric face id
-face_id = 1
+    print("\n [INFO] Inicializando processo de captura ...")
+    # Initialize individual sampling face count
+    count = 0
 
-print("\n [INFO] Inicializando processo de captura ...")
-# Initialize individual sampling face count
-count = 0
+    while(True):
 
-while(True):
+        ret, img = cam.read()
+        #img = cv2.flip(img, -1) # flip video image vertically
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        faces = face_detector.detectMultiScale(gray, 1.3, 5)
 
-    ret, img = cam.read()
-    #img = cv2.flip(img, -1) # flip video image vertically
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    faces = face_detector.detectMultiScale(gray, 1.3, 5)
+        for (x,y,w,h) in faces:
 
-    for (x,y,w,h) in faces:
+            cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
+            count += 1
 
-        cv2.rectangle(img, (x,y), (x+w,y+h), (255,0,0), 2)     
-        count += 1
+            # Save the captured image into the datasets folder
+            cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
 
-        # Save the captured image into the datasets folder
-        cv2.imwrite("dataset/User." + str(face_id) + '.' + str(count) + ".jpg", gray[y:y+h,x:x+w])
+            cv2.imshow('image', img)
 
-        cv2.imshow('image', img)
+        k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
+        if k == 27:
+            break
+        elif count >= 30: # Take 30 face sample and stop video
+            break
 
-    k = cv2.waitKey(100) & 0xff # Press 'ESC' for exiting video
-    if k == 27:
-        break
-    elif count >= 30: # Take 30 face sample and stop video
-        break
-
-# Do a bit of cleanup
-print("\n [INFO] Finalizando Programa")
-cam.release()
-cv2.destroyAllWindows()
+    # Do a bit of cleanup
+    print("\n [INFO] Finalizando Programa")
+    cam.release()
+    cv2.destroyAllWindows()
+    return True
 
 
