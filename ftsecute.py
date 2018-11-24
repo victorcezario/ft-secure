d8ba3c5d8ea70382669c1fbdcb633d0a7baa34e2
unknown <victorhcezario@gmail.com>
2018-11-23 22:29:31 -0200

    changes

--- ftsecute.py
+++ ftsecute.py
@@ -1,17 +1,12 @@
 # coding=utf-8
 
-#Bibliotecas OpenCV
-import faceDataset
-import faceTraining
-import faceRecognition
-#import nokia
-
 #ESPEAK
-from num2words import num2words
-from subprocess import call
+
 
 from gpiozero import Button
 from signal import pause
+from num2words import num2words
+from subprocess import call
 #Function falar palavras
 def speakText(texto):
     cmd_beg= 'espeak -vpt-br "'
